be09894f91aa5afac8f4b6aa5960047fa7f245ad
jonesdc76 <jonesdc76@gmail.com>
2017-06-23 12:20:47 -0400

    some macros for finding corrections as function of T and H

---
+++ CorrectionsMagnetization.C
@@ -0,0 +1,78 @@
+/*
+Don Jones, June 2017
+...........................
+
+Calculate magnetization corrections as a function of temperature and B-field
+Follows treatment in Equations 4 and 7 in 
+"Spin-waves in nickel, iron and yttrium-iron garnet", Pauthenet, March 1982
+
+Note: Start with room-temperature measurements of magnetization at 1 Telsa
+      then only need to calculate the small corrections from increases in
+      temperature and applied B-field. These corrections are given by dM/dH
+      in Eq 7, but since the values of A and B are not given for room 
+      temperature we will have to differentiate Eq 9 w.r.t. H and T. The 
+      internal field H_i in these equations is the total field B including the
+      applied field plus magnetization but above the saturation magnetization
+      dM/dB=dM/dH_i=dM/dH. Slopes are assumed to be linear over the small ranges
+      of temperature and magnetization such that (delta M) = dM/dH*(delta H) 
+      and (delta M) = dM/dT*(delta T)
+
+Arguments: meas_T is the temperature in Kelvin at which the magnetization 
+           is known from measurement(assumed to be room temperature)
+
+           meas_Hi is the internal field in Oersteds at which the magnetization 
+	   was measured (If the applied field is assumed to be 10000 Oersteds =
+	   1 Telsa, then the internal field is 17177 Oe. Above saturation, the
+           relation is a constant offset of 7.177 Oe. Below saturation the
+           relationship is not so simple and this program is insufficient.)
+
+Return:  total correction to be ADDED to starting magnetization to correct
+         for effects from temperature and applied field increases
+
+NOTE on translating from internal field H_i to applied field H.
+-------------------------------------------------------------------------------
+      The Pauthenet paper calculates corrections in terms of internal field H_i.
+      The internal field H_i is the difference between the applied field & the  
+      demagnetizing field (https://en.wikipedia.org/wiki/Demagnetizing_field)
+      H_i = H - gM (emu system) where g is the demagnetizing factor and M is  
+      magnetization. For spheres in a uniform applied field H, g = 4pi/3 giving       H_i = H - 4piM/3 (see eq 5.98 Jackson 2nd edition). Pauthenet used 5 mm  
+      diameter spheres in this measurement and since we generally want to talk 
+      in terms of applied field we can use H = H_i+M/3 to translate his 
+      corrections to be in terms of the applied field H. Above saturation, the 
+      magnetization is approximately constant. For iron the saturation 
+      magnetization is given as 217.6+/-0.1 emu/g at T=293 K and H=10 kOe in 
+      Crangle and Goodman(1970) so 4piM/3= 7.177 kOe. Thus, to translate 
+      from internal field to H you add 7.177 kOe. My default field is H=10 kOe 
+      which gives H_i=17.72 kOe and my default temperature is 293 K.
+*/ 
+#include <iostream>
+#include <cstdio>
+const double a3_2 = 307e-6, a5_2 = 22.8e-8, b = 1.378e-4;
+const double M43rdsPi = 7177; //magnetization of Iron in Oersted. Add this to
+                              // H_i to get applied field H above saturation.
+double CorrectionsMagnetization(double dT, double dH, //in Kelvin and Oersted
+				double meas_T = 293, double meas_Hi = 17177){
+
+  double dM = 0, M_start, M_final;
+  //calculate 307e-6*T^(3/2)*F(3/2, 1.378*Hi/T) from equation (9) at meas_T
+  //and meas_Hi
+  double s = 3.0/2.0, term1 = 0, term2 = 0, term3 = 0;
+  for(int i=1;i<10000;++i){
+    term1 += a3_2*pow(meas_T,s)*pow(i,-s)*exp(-i*b*meas_Hi/meas_T);
+    term1 -= a3_2*pow(meas_T+dT,s)*pow(i,-s)*exp(-i*b*(meas_Hi+dH)/(meas_T+dT));
+  }
+  s = 5.0/2.0;
+  for(int i=1;i<10000;++i){
+    term2 += a5_2*pow(meas_T,s)*pow(i,-s)*exp(-i*b*meas_Hi/meas_T);
+    term2 -= a5_2*pow(meas_T+dT,s)*pow(i,-s)*exp(-i*b*(meas_Hi+dH)/(meas_T+dT));
+    
+  }
+  //my linear parameterization of Chi term using fit to values in Table 1
+  double intercept = 3.644e-6, slope = 5.0434e-10;
+  term3 += intercept*dH + slope * (dT*meas_Hi + (meas_T+dT)*dH);
+  cout<<"Term 1:"<<term1<<endl;
+  cout<<"Term 2:"<<term2<<endl;
+  cout<<"Term 3:"<<term3<<endl;
+  dM = term1+term2+term3;
+  return dM;
+}
---
+++ Magnetization_vs_T.C
@@ -0,0 +1,74 @@
+{
+//This graph comes from Crangle and Goodman 1970 "The Magnetization of Pure Iron and Nickel"
+  bool useFe = 1;
+  const int N=40;
+  const double conversion = 0.00999921;//convert from emu/g to emu/atom
+  const double CurieTempFe = 1044.0;//Curie temperature of Iron in Kelvin
+  const double CurieTempNi = 631.0;//Curie temperature of Nickel in Kelvin
+  const double satMag0KFe = 221.71;
+  const double satMag0KNi = 58.58;
+  double absMagFe[N] = {221.7,221.7,221.6,221.5,221.3,221.1,220.9,220.6,220.3,219.9,219.6,219.1,218.7,218.2,217.6,217.2,216.7,216.1,215.5,214.7,213.8,212.9,212.0,210.9,209.8,208.4,207.1,205.7,204.2,202.6,200.8,199.0,196.9,195.0,192.7,190.3,187.6,184.8,181.5,177.9};
+  double absMagNi[N] = {58.58,58.56,58.52,58.48,58.43,58.36,58.3,58.22,58.14,58.06,57.95,57.82,57.68,57.54,57.39,57.22,57.04,56.84,56.61,56.38,56.11,55.81,55.51,55.18,54.68,54.29,53.9,53.44,52.99,52.52,52.03,51.56,51.08,50.57,50.02,49.32,48.53,47.62,46.5,45.17};
+  double reducedTemp[N], tempFe[N], reducedMagFe[N], tempNi[N], reducedMagNi[N];
+  for(int i=0;i<N;++i){
+    reducedTemp[i] = (i*0.02);
+    reducedMagFe[i] = absMagFe[i]/satMag0KFe;
+    reducedMagNi[i] = absMagNi[i]/satMag0KNi;
+    tempFe[i] = reducedTemp[i]*CurieTempFe;
+    tempNi[i] = reducedTemp[i]*CurieTempNi;
+    if(useFe)
+      printf("%0.2f  %0.4f | %0.1f  %0.1f\n",reducedTemp[i], reducedMagFe[i],tempFe[i], absMagFe[i]);
+    else
+      printf("%0.2f  %0.4f | %0.1f  %0.1f\n",reducedTemp[i], reducedMagNi[i],tempNi[i], absMagNi[i]);
+  }
+  TF1 *f = new TF1("f","pol1",270,340);
+  TGraph *gr;
+  if(useFe)
+    gr = new TGraph(N, tempFe, absMagFe);
+  else
+    gr = new TGraph(N, tempNi, absMagNi);
+  gr->SetMarkerColor(kBlue);
+  gr->SetMarkerStyle(8);
+  gr->SetMarkerSize(0.8);
+  gr->SetLineColor(kRed);
+  gr->Fit(f,"r");
+  TPaveText *pt = new TPaveText(0.5,0.8,0.89,0.89,"ndc");
+  pt->SetShadowColor(0);
+  pt->SetFillColor(0);
+  pt->SetBorderSize(0);
+  pt->AddText("Temperature Sensitivity at 294 K");
+  double sens = f->GetParameter(1)/(f->GetParameter(0)+ 294*f->GetParameter(1));
+  pt->AddText(Form("%0.4f\%#^{o}C", sens*100));
+  if(useFe)
+    gr->SetTitle("Temperature Dependence of Saturation Magnetization for Pure Iron");
+  else
+    gr->SetTitle("Temperature Dependence of Saturation Magnetization for Pure Nickel");
+  gr->GetXaxis()->SetTitle("Temperature (K)");
+  gr->GetYaxis()->SetTitle("Saturation Magnetization (emu/g)");
+  gr->Draw("ap");
+  pt->Draw();
+  if(0){
+    TGraph *grRedFe = new TGraph(N, reducedTemp, reducedMagFe);
+    grRedFe->SetMarkerColor(kBlue);
+    grRedFe->SetMarkerStyle(8);
+    grRedFe->SetMarkerSize(0.8);
+    grRedFe->SetLineColor(kRed);
+    grRedFe->SetTitle("Temperature Dependence of Saturation Magnetization for Pure Iron(Blue) and Nickel(Red)");
+    grRedFe->GetXaxis()->SetTitle("Reduced Temperature");
+    grRedFe->GetYaxis()->SetTitle("Reduced Saturation Magnetization");
+    grRedFe->Draw("ap");
+    TGraph *grRedNi = new TGraph(N, reducedTemp, reducedMagNi);
+    grRedNi->SetMarkerColor(kRed);
+    grRedNi->SetMarkerStyle(8);
+    grRedNi->SetMarkerSize(0.8);
+    grRedNi->Draw("samep");
+  }
+  double T_chi[11] = {4.21,24.79,51.05,75.34,100.51,131.55,165.81,197.45,226.34,254.53,286.41};
+  double chi[11] = {3.61,3.71,3.70,3.76,3.67,3.64,3.66,3.66,3.79,3.83,3.82};
+  for(int i=0;i<11;++i)chi[i]*=1e-6;
+  TGraph *grChi = new TGraph(11,T_chi,chi);
+  grChi->SetMarkerColor(kRed);
+  grChi->SetMarkerStyle(8);
+  grChi->Draw("alp");
+}
+
