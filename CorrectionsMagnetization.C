861b8e7466f3043e12d4e501e2b5d7d929938e8b
jonesdc76 <jonesdc76@gmail.com>
2017-07-14 17:17:39 -0400

    Update to code and pol manuscript

--- CorrectionsMagnetization.C
+++ CorrectionsMagnetization.C
@@ -21,10 +21,11 @@ Arguments: meas_T is the temperature in Kelvin at which the magnetization
            is known from measurement(assumed to be room temperature)
 
            meas_Hi is the internal field in Oersteds at which the magnetization 
-	   was measured (If the applied field is assumed to be 10000 Oersteds =
-	   1 Telsa, then the internal field is 17177 Oe. Above saturation, the
-           relation is a constant offset of 7.177 Oe. Below saturation the
-           relationship is not so simple and this program is insufficient.)
+	   was measured (Above saturation, the relation is a nearly constant 
+	   offset of H = H_i+7.177 Oe for Fe. Below saturation the relationship
+	   is not so simple and this program is insufficient. If the applied 
+	   field is assumed to be 10000 Oersteds = 1 Telsa, then the internal 
+	   field H_i = 2823 Oe.)
 
 Return:  total correction to be ADDED to starting magnetization to correct
          for effects from temperature and applied field increases
@@ -37,13 +38,14 @@ NOTE on translating from internal field H_i to applied field H.
       H_i = H - gM (emu system) where g is the demagnetizing factor and M is  
       magnetization. For spheres in a uniform applied field H, g = 4pi/3 giving       H_i = H - 4piM/3 (see eq 5.98 Jackson 2nd edition). Pauthenet used 5 mm  
       diameter spheres in this measurement and since we generally want to talk 
-      in terms of applied field we can use H = H_i+M/3 to translate his 
+      in terms of applied field we can use H = H_i+4piM/3 to translate his 
       corrections to be in terms of the applied field H. Above saturation, the 
       magnetization is approximately constant. For iron the saturation 
-      magnetization is given as 217.6+/-0.1 emu/g at T=293 K and H=10 kOe in 
-      Crangle and Goodman(1970) so 4piM/3= 7.177 kOe. Thus, to translate 
-      from internal field to H you add 7.177 kOe. My default field is H=10 kOe 
-      which gives H_i=17.72 kOe and my default temperature is 293 K.
+      magnetization is given as 217.6+/-0.1 emu/g  at T=293 K and H=10 kOe in 
+      Crangle and Goodman(1970)which is equivalent to 4pirho(217.6)=21.53 kOe 
+      (rho is density in g/cm^3) so 4piM/3= 7.177 kOe. Thus, to translate from 
+      internal field to H you add 7.177 kOe. My default field is H=10 kOe which
+      gives H_i=10.000-7.177 kOe and my default temperature is 293 K.
 */ 
 #include <iostream>
 #include <cstdio>
@@ -51,7 +53,7 @@ const double a3_2 = 307e-6, a5_2 = 22.8e-8, b = 1.378e-4;
 const double M43rdsPi = 7177; //magnetization of Iron in Oersted. Add this to
                               // H_i to get applied field H above saturation.
 double CorrectionsMagnetization(double dT, double dH, //in Kelvin and Oersted
-				double meas_T = 293, double meas_Hi = 17177){
+				double meas_T = 293, double meas_Hi = 2823){
 
   double dM = 0, M_start, M_final;
   //calculate 307e-6*T^(3/2)*F(3/2, 1.378*Hi/T) from equation (9) at meas_T
